fd88f5f93c2585d45d14ee4dd73f3123170387d2
Jin <fauzipribadi18@gmail.com>
2025-09-22 00:32:59 +0800

    feat(theme): add text styles and update color scheme

--- lib/cores/constants/colors.dart
+++ lib/cores/constants/colors.dart
@@ -1,17 +1,11 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  /// primary = #025E49
-  static const Color primary = Color(0xff025E49);
+  /// primary = #FAA533
+  static const Color primary = Color(0xffFAA533);
 
-  /// grey = #B7B7B7
-  static const Color grey = Color(0xffB7B7B7);
-
-  /// light = #F8F5FF
-  static const Color light = Color(0xffF8F5FF);
-
-  /// light = #C7D0EB
-  static const Color blueLight = Color(0xffC7D0EB);
+  /// body = #F8F5FF
+  static const Color body = Color(0xffF8F5FF);
 
   /// black = #000000
   static const Color black = Color(0xff000000);
@@ -19,11 +13,20 @@ class AppColors {
   /// white = #FFFFFF
   static const Color white = Color(0xffFFFFFF);
 
+  /// grey = #B7B7B7
+  static const Color grey = Color(0xffB7B7B7);
+
   /// green = #50C474
   static const Color green = Color(0xff50C474);
 
   /// red = #F4261A
   static const Color red = Color(0xffF4261A);
+
+  /// blue = #1E90FF
+  static const Color blue = Color(0xff1E90FF);
+
+  // orange = #EF7722
+  static const Color orange = Color(0xffEF7722);
 
   /// card = #E5E5E5
   static const Color card = Color(0xffE5E5E5);
